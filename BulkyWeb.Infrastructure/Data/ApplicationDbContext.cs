f3a8bc3b6793ee94e0b06f01dce7a80e4bcdf8b2
sun <patsun1993@gmail.com>
2023-12-04 23:34:10 +0900

    Seeder

--- BulkyWeb.Infrastructure/Data/ApplicationDbContext.cs
+++ BulkyWeb.Infrastructure/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BulkyWeb.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Infrastructure.Data
@@ -12,6 +13,25 @@ namespace BulkyWeb.Infrastructure.Data
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
 
+    }
+
+    // automatically generate
+    public DbSet<Villa> Villas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      //base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Villa>().HasData(new Villa
+      {
+        Id = 1, 
+        Name = "a",
+        Description = "des",
+        ImageUrl = "https://placehold.co/600*402",
+        Occupancy = 2,
+        Price = 200,
+        Sqft = 500
+      });
     }
   }
 }
---
+++ BulkyWeb.Infrastructure/Migrations/20231204143011_seedVillaToDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[] { 1, null, "des", "https://placehold.co/600*402", "a", 2, 200.0, 500, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
