d85086e6481552c64318bb47402b62dda7578bf5
sun <patsun1993@gmail.com>
2023-12-16 22:55:59 +0900

    use generic

--- BulkyWeb.Infrastructure/Repository/VillaRepository.cs
+++ BulkyWeb.Infrastructure/Repository/VillaRepository.cs
@@ -11,62 +11,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Infrastructure.Repository
 {
-  public class VillaRepository : IVillaRepository
+  public class VillaRepository : Repository<Villa>, IVillaRepository
   {
     private readonly ApplicationDbContext _db;
 
-    public VillaRepository(ApplicationDbContext db) => _db = db;
-
-    public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-    {
-      IQueryable<Villa> query = _db.Set<Villa>();
-      if (filter != null)
-      {
-        query = query.Where(filter);
-      }
-
-      if (!string.IsNullOrEmpty(includeProperties))
-      {
-        // Case Sensitive: Villa, VillaNumber
-        foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
-        {
-          query = query.Include(includeProperty);
-        }
-      }
-
-      return query.ToList();
-    }
-
-    public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-    {
-      IQueryable<Villa> query = _db.Set<Villa>();
-      query = query.Where(filter);
-
-      if (!string.IsNullOrEmpty(includeProperties))
-      {
-        // Case Sensitive: Villa, VillaNumber
-        foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-        {
-          query = query.Include(includeProperty);
-        }
-      }
-
-      return query.FirstOrDefault();
-    }
-
-    public void Add(Villa entity)
-    {
-      _db.Add(entity);
-    }
+    public VillaRepository(ApplicationDbContext db) : base(db) => _db = db;
 
     public void Update(Villa entity)
     {
       _db.Update(entity);
-    }
-
-    public void Remove(Villa entity)
-    {
-      _db.Remove(entity);
     }
 
     public void Save()
