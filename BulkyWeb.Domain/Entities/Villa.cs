ca4b7451622c7591e563788106c840b0e28f9bc6
sun <patsun1993@gmail.com>
2023-12-21 22:01:59 +0900

    upload image

--- BulkyWeb.Domain/Entities/Villa.cs
+++ BulkyWeb.Domain/Entities/Villa.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace BulkyWeb.Domain.Entities
 {
@@ -13,10 +15,14 @@ namespace BulkyWeb.Domain.Entities
     public required string Name { get; set; }
     [Display(Name = "Descriptions")]
     public string? Description { get; set; }
+    [Range(10, 10000)]
     public double Price { get; set; }
     public int Sqft { get; set; }
+    [Range(1, 10)]
     public int Occupancy { get; set; }
     public string? ImageUrl { get; set; }
+    [NotMapped]
+    public IFormFile? Image { get; set; }
     public DateTime? CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
   }
--- BulkyWeb/Controllers/VillaController.cs
+++ BulkyWeb/Controllers/VillaController.cs
@@ -1,16 +1,19 @@
 ﻿using BulkyWeb.Application.Common.Interfaces;
 using BulkyWeb.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace BulkyWeb.Controllers;
 
 public class VillaController : Controller
 {
   private readonly IUnitOfWork _unitOfWork;
+  private readonly IWebHostEnvironment _webHostEnvironment;
 
-  public VillaController(IUnitOfWork unitOfWork)
+  public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
   {
     _unitOfWork = unitOfWork;
+    _webHostEnvironment = webHostEnvironment;
   }
 
   public IActionResult Index()
@@ -32,6 +35,22 @@ public class VillaController : Controller
 
     if (ModelState.IsValid)
     {
+      if (obj.Image != null)
+      {
+        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+        string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+
+        using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+        obj.Image.CopyTo(fileStream);
+
+        obj.ImageUrl = @"\images\VillaImage\" + fileName;
+      }
+      else
+      {
+        obj.ImageUrl = "https://placehold.co/600*400";
+      }
+
+
       _unitOfWork.Villa.Add(obj);
       _unitOfWork.Save();
 
